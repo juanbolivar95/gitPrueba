fd2589e536992e68066ee618fedece07f4324f6c
Juan David Bolivar <judaob95@gmail.com>
2020-05-23 17:14:14 -0500

    Mi primer proyecto

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <div class="container py-3">
+      
+        <div class="card-container centermx drop-shadow lifted">
+
+          <div class="card">
+          
+            <div class="row">
+                
+                <div class="row">
+                  <div class="col-md-5">
+                    <div class="foto">
+                      <img src="img/foto2.jpg" class="img-fluid img-thumbnail" alt="..." width="120px" height="100px">
+                    </div>
+                  </div>
+                  <div class="col-md-7">
+                    <div class="logo">
+                      <img src="img/logo_cri.jpeg" class="img-fluid img-thumbnail logo" alt="..." width="120px" height="100px">
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="row">
+                  <div class="col-md-5 text-center">
+                    <h5>Juan David Bolivar</h5>
+                    <small>Ingeniero Civil</small>
+                  </div>
+                  <div class="col-md-7" style="font-size: 14px; margin: 4px -33px">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <p>Telefono: <br> <small id="address_carnet">312 710 9900</small> </p>
+                      </div>
+                      <div class="col-md-6">
+                        <p>Cedula: <br> <small id="address_carnet">1114245633</small> </p>
+                      </div>
+                      <div class="col-md-6">
+                        <p>Direccion: <br> <small id="address_carnet">Calle 12 # 10-45</small> </p>
+                      </div>
+                      <div class="col-md-6">
+                        <p>obra: <br> <small id="address_carnet">Contructora Bolivar</small> </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+            </div>
+
+          </div>
+
+            <div class="pin float-right">
+              <img src="img/pin.png" class="img-fluid img-thumbnail" alt="..." width="50px" height="100px">
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container py-3">
+      
+        <div class="card-container centermx drop-shadow lifted">
+
+          <div class="card-trasera">
+          
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text"></p>
+            ________________________________
+          </div>
+          <div class="card-footer text-muted">
+            Este carné identifica al portador como trabajador Válido hasta el 31 de diciembre del 2021
+          </div>
+
+          </div>
+
+        </div>
+
+        
+            
+    </div>
+
+
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
